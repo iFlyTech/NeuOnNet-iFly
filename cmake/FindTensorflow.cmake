158c4457ee98fa653bf93d059e808665ca0a4dde
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-05-03 17:01:35 +0000

    manual update

--- cmake/FindTensorflow.cmake
+++ cmake/FindTensorflow.cmake
@@ -15,4 +15,27 @@ if(${PKG_CONFIG_FOUND})
     endif ()
 endif()
 
-find_path(Tensorflow_INCLUDE_DIR NAMES tensorflow/c/c_api.h PATH_SUFFIXES tensorflow tensorflow/include HINTS 
+find_path(Tensorflow_INCLUDE_DIR NAMES tensorflow/c/c_api.h PATH_SUFFIXES tensorflow tensorflow/include HINTS ${Tensorflow_INCLUDE_HINTS})
+find_library(Tensorflow_LIBRARY NAMES tensorflow ${Tensorflow_LIB_NAMES} PATH_SUFFIXES tensorflow tensorflow/lib HINTS ${Tensorflow_LIB_HINTS})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Tensorflow DEFAULT_MSG Tensorflow_INCLUDE_DIR Tensorflow_LIBRARY)
+
+if (Tensorflow_FOUND)
+    mark_as_advanced(
+        Tensorflow_LIBRARY
+        Tensorflow_INCLUDE_DIR)
+
+    message(STATUS "Tensorflow will be used from ${Tensorflow_INCLUDE_DIR} with libs ${Tensorflow_LIBRARY}")
+
+    add_library(Tensorflow::Tensorflow INTERFACE IMPORTED)
+    set_target_properties(Tensorflow::Tensorflow PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${Tensorflow_INCLUDE_DIR}"
+        INTERFACE_LINK_LIBRARIES "${Tensorflow_LIBRARY}"
+        INTERFACE_COMPILE_OPTIONS "${Tensorflow_CFLAGS};${Tensorflow_CFLAGS_OTHER}"
+        )
+else()
+    message(FATAL_ERROR "Tensorflow libraries were not found at ${Tensorflow_INCLUDE_DIR}, try to specify Tensorflow_ROOT variable to point to Tensorflow root folder")
+endif()
+
+cmake_policy(POP)
