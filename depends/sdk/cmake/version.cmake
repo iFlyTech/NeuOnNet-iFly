894139f6857fdafb833d7c581116f2bba74f63f8
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-05-17 14:40:47 +0000

    translations: update Korean

--- depends/sdk/cmake/version.cmake
+++ depends/sdk/cmake/version.cmake
@@ -3,3 +3,21 @@ function(configure_version symbols_space_name)
     if (NOT DEFINED version)
         set(version 0.0.0 CACHE INTERNAL "Version String")
     endif ()
+    if (NOT DEFINED revision)
+        set(revision 0000000 CACHE INTERNAL "Revision String")
+    endif ()
+    if (NOT DEFINED symbols_space_name)
+        message(FATAL_ERROR "You have to define variable named symbols_space_name to be able to use automatic version string generator")
+    endif ()
+
+    set(version_revision ${version}+g${revision} CACHE INTERNAL "Version Revision string")
+
+    configure_file(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../res/version.h.in ${CMAKE_CURRENT_BINARY_DIR}/generated/version.h @ONLY)
+    configure_file(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../res/version.cpp.in ${CMAKE_CURRENT_BINARY_DIR}/generated/version.cpp @ONLY)
+
+    message(STATUS "You are able to use fully qualified variable version_revision to refer to version and revision")
+    message(STATUS "Don't forget to add CMAKE_CURRENT_BINARY_DIR/generated (which is ${CMAKE_CURRENT_BINARY_DIR}/generated) to your target include directories with PRIVATE BUILD_INTERFACE")
+endfunction()
+
+function(configure_birthday symbols_space_name)
+    if (NOT DEFINED symbols_spa
