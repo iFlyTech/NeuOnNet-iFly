ab9a2f5c52234fc77e720a9c26093764230f5653
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-05-17 16:36:18 +0000

    Some fixes.

--- depends/sdk/cmake/version.cmake
+++ depends/sdk/cmake/version.cmake
@@ -20,4 +20,16 @@ function(configure_version symbols_space_name)
 endfunction()
 
 function(configure_birthday symbols_space_name)
-    if (NOT DEFINED symbols_spa
+    if (NOT DEFINED symbols_space_name)
+        message(FATAL_ERROR "You have to define variable named symbols_space_name to be able to use automatic version string generator")
+    endif ()
+
+    string(TIMESTAMP birthday_string UTC)
+    set(birthday ${birthday_string} CACHE INTERNAL "The date of birth")
+
+    configure_file(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../res/birthday.h.in ${CMAKE_CURRENT_BINARY_DIR}/generated/birthday.h)
+    configure_file(${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../res/birthday.cpp.in ${CMAKE_CURRENT_BINARY_DIR}/generated/birthday.cpp)
+
+    message(STATUS "You are able to use variable birthday to refer to build date")
+    message(STATUS "Don't forget to add CMAKE_CURRENT_BINARY_DIR/generated (which is ${CMAKE_CURRENT_BINARY_DIR}/generated) to your target include directories with PRIVATE BUILD_INTERFACE")
+endfunction()
