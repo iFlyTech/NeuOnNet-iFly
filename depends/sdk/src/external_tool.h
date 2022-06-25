941ff4041860b86ef2d4edf242f7e46dfabbf0d5
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-06-25 19:38:13 +0000

    Few bug fixes

--- depends/sdk/src/external_tool.h
+++ depends/sdk/src/external_tool.h
@@ -9,4 +9,22 @@
 namespace posix{
     class external_tool_t {
         static const pid_t child_pid_default{0};
-        static const pid_t chil
+        static const pid_t child_pid_error{-1};
+    public:
+        struct exit_status_t{
+            bool has_terminated{false};
+            int code{0};
+        };
+    public:
+        explicit external_tool_t(const std::string &exec);
+        exit_status_t execute(const std::vector<std::string> &args, int pipe);
+        void interrupt() const;
+
+    protected:
+        static std::vector<const char *> arguments(const std::string& exec, const std::vector<std::string> &args);
+
+    private:
+        const std::string exec;
+        boost::optional<pid_t> child_pid;
+    };
+}
