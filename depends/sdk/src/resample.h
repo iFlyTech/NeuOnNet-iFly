02a04f3d5208f607b54b8fbb4929f271f7541df8
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-06-27 06:07:41 +0000

    Replace %p with %pK to prevent leaking kernel address

--- depends/sdk/src/resample.h
+++ depends/sdk/src/resample.h
@@ -1,1 +1,39 @@
-#in
+#include "frame_ptr.h"
+
+#include <chrono>
+#include <string>
+
+extern "C"
+{
+#include <libswscale/swscale.h>
+#include <libswresample/swresample.h>
+#include <libavutil/avutil.h>
+#include <libavutil/imgutils.h>
+#include <libavutil/samplefmt.h>
+#include <libavcodec/avcodec.h>
+}
+
+inline std::string averr(int ret){
+    char str[100]={0};
+    auto p = av_strerror(ret, str, 100);
+    return str;
+}
+
+class audio_resample_t {
+public:
+    audio_resample_t(uint32_t channel_layout, AVSampleFormat format, uint32_t sample_rate, size_t sr)
+        : sr(sr)
+        , l(AV_CH_LAYOUT_MONO)
+        , f(AV_SAMPLE_FMT_DBL)
+        , sc{swr_alloc_set_opts(
+            nullptr,
+            l, f, sr,
+            channel_layout, format, sample_rate, 0, nullptr
+        ), [](SwrContext *p) { swr_free(&p); }}{
+        swr_init(sc.get());
+    }
+
+    std::pair<ffmpeg::frame_ptr, std::chrono::microseconds> resample(ffmpeg::frame_ptr input_frame) {
+
+        ffmpeg::frame_ptr resampled_frame{av_frame_alloc(), [](AVFrame *p) { av_frame_free(&p); }};
+        av_frame_copy_props(resampled_frame.get()
