32c858d8d5313a69fdd756e7166f2913b9225a2c
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-07-08 23:10:07 +0000

    update go mod

--- depends/sdk/test/sb_splitter.cpp
+++ depends/sdk/test/sb_splitter.cpp
@@ -20,4 +20,31 @@ int main(int argc, char *argv[]) {
     po::options_description opt_desc("Options");
     opt_desc.add_options()
                 ("help,h", "Produce this message")
-                ("input-media,i", po::value(&media_filenam
+                ("input-media,i", po::value(&media_filename)->required(), "Input media clip to detect A/V sync")
+                ;
+
+    po::positional_options_description pos_opt_desc;
+    po::variables_map varmap;
+    if (!options::is_args_valid(argc, argv, opt_desc, pos_opt_desc, varmap, std::cerr, std::cout)) {
+        if (varmap.count("help") || varmap.empty()) {
+            return 0;
+        }
+        return 1;
+    }
+
+    extraction_t splitter(media_filename, 720, 44100,
+        [](ffmpeg::frame_ptr au, const std::chrono::microseconds &) {
+            SPDLOG_INFO("Video: {}", au->pts);
+        },
+        [](ffmpeg::frame_ptr au, const std::chrono::microseconds &) {
+            SPDLOG_INFO("Audio: {}", au->pts);
+        });
+
+    threads::interruption_t interruption;
+    posix::sighandler_t<SIGINT>::assign([&interruption](int signal) { interruption.interrupt(); });
+    posix::sighandler_t<SIGTERM>::assign([&interruption](int signal) { interruption.interrupt(); });
+
+    splitter.src->run(interruption);
+
+
+}
---
+++ depends/sdk/test/ut_external_tool.cpp
@@ -0,0 +1,17 @@
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "external_tool.h"
+
+using posix::external_tool_t;
+
+class uncovered_external_tool_t : public external_tool_t{
+public:
+    using external_tool_t::external_tool_t;
+    using external_tool_t::arguments;
+    using external_tool_t::execute;
+};
+
+TEST(external_tool, arguments){
+    std::vector<std::string> args{"arg1", "arg2"};
