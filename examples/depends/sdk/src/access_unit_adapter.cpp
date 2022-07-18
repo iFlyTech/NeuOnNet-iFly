80714a1aad9edc29e8735392b25240e3c3b48832
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-07-18 20:55:10 +0000

    commit blogs

---
+++ examples/depends/sdk/src/access_unit_adapter.cpp
@@ -0,0 +1,7 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+#include "access_unit_adapter.h"
+
+using namespace ffmpeg;
+
+const AVRational access_unit_adapter::Microseconds{std:
