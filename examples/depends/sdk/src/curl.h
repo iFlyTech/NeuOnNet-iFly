e4a56844c7789bb0d2e280bd624e95543e092be0
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-07-20 06:08:45 +0000

    spi: dw-mid: terminate ongoing transfers at exit

--- examples/depends/sdk/src/curl.h
+++ examples/depends/sdk/src/curl.h
@@ -18,4 +18,16 @@ namespace http{
         explicit curl_t(const std::string &base_url);
 
         response_t post(const std::string &url, const std::string &body = std::string(), const std::map<std::string, std::string> &headers = std::map<std::string, std::string>());
-        response_t get(const std::string &url, const std::map<std::string, 
+        response_t get(const std::string &url, const std::map<std::string, std::string> &headers = std::map<std::string, std::string>());
+        response_t put(const std::string &url, const std::string &body = std::string(), const std::map<std::string, std::string> &headers = std::map<std::string, std::string>());
+
+    protected:
+        response_t execute(CURL *curl, const std::string &url, const std::map<std::string, std::string> &headers);
+        static std::unique_ptr<curl_slist, decltype(&curl_slist_free_all)> make_headers_list(const std::map<std::string, std::string> &headers);
+        static size_t write_callback(const void *const data, size_t size, size_t nmemb, void *userdata);
+        static size_t read_callback(void *const data, size_t size, size_t nmemb, void *userdata);
+        static size_t header_callback(const void *const data, size_t size, size_t nmemb, void *userdata);
+        static std::string endpoint(const std::string &base_url, const std::string &url);
+        static std::string sanitize_base_url(const std::string url);
+        static std::string sanitize_entrypoint_url(const std::string url);
+        static response_t sanitize_response(const CURLcode code, con
