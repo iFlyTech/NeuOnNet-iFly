ba01fe885f76d1568a956fc2a370487b06fc0646
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-08-09 07:04:41 +0000

    First public release.

--- examples/depends/sdk/src/external_tool.cpp
+++ examples/depends/sdk/src/external_tool.cpp
@@ -32,4 +32,36 @@ external_tool_t::exit_status_t external_tool_t::execute(const std::vector<std::s
 
         auto argv = arguments(exec, args);
         // Never returns in case of success, exit child process in case of failure
-        
+        // Be aware - we cast pointer to const value to const pointer to value;
+        // it would be nice to have non-const allocated memory to avoid explicit const_cast
+        execve(exec.c_str(), const_cast<char *const *>(argv.data()), nullptr);
+        exit(errno);
+    } else if (*child_pid) {
+        bool is_waiting(true);
+        do {
+            waitpid(*child_pid, &status, WUNTRACED | WCONTINUED);
+
+            if (WIFEXITED(status)) {
+                is_waiting = false;
+                ret.code = WEXITSTATUS(status);
+            } else if (WIFSIGNALED(status)) {
+                is_waiting = false;
+                ret.has_terminated = true;
+                ret.code = WTERMSIG(status);
+            }
+        } while (is_waiting);
+    }
+
+    return ret;
+}
+
+void external_tool_t::interrupt() const {
+    if( child_pid ){
+        kill(*child_pid, SIGINT);
+    }
+}
+
+std::vector<const char *> external_tool_t::arguments(const std::string& exec, const std::vector <std::string> &args) {
+    std::vector<const char *> argv;
+    argv.push_back(exec.c_str());
+    std::transform(args.begin(), args
