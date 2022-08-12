8073eb112379081362f7aa8488f55ef1771e15a9
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-08-12 19:52:46 +0000

    Update draft release notes to 1.8.2

--- examples/depends/sdk/src/hash_fnv1a.h
+++ examples/depends/sdk/src/hash_fnv1a.h
@@ -7,3 +7,12 @@
 namespace algorithm{
     namespace detail{
         constexpr std::uint32_t fnv1a_32(const char* s, std::size_t count) {
+            return ((count ? fnv1a_32(s, count - 1) : 2166136261u) ^ static_cast<uint32_t >(s[count])) * 16777619u;
+        }
+    }
+
+    constexpr uint32_t operator "" _H(const  char *s, std::size_t count) {
+        return detail::fnv1a_32(s, count);
+    }
+
+}
