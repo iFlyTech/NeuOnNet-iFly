289cb10d4bca4004b1f508c74f6edb73d0343a6f
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-10-02 17:38:25 +0000

    Updated test.md

--- examples/depends/sdk/test/ut_ntp.cpp
+++ examples/depends/sdk/test/ut_ntp.cpp
@@ -19,4 +19,44 @@ TEST(ntp, unpack){
 
 TEST(ntp, from_iso8601){
 
-    ntp_t value = from_iso8601("1900-01-0
+    ntp_t value = from_iso8601("1900-01-01 00:00:01");
+    ASSERT_EQ(1, value.seconds);
+
+    value = from_iso8601("1900-01-01 10:00:00");
+    ASSERT_EQ(36000, value.seconds);
+
+    value = from_iso8601("1900-01-02 00:00:00");
+    ASSERT_EQ(86400, value.seconds);
+
+    value = from_iso8601("1900-01-01 00:00:00.5");
+    ASSERT_EQ(0, value.seconds);
+    ASSERT_EQ(value.fractions_per_second / 2,  value.fraction);
+
+    value = from_iso8601("1900-01-01 00:00:00.000001");
+    ASSERT_EQ(0, value.seconds);
+    ASSERT_EQ(value.fractions_per_second / 1000000, value.fraction);
+}
+
+TEST(ntp, with_delimiter){
+    ntp_t value = from_iso8601("1900-01-01T00:00:01");
+    ASSERT_EQ(1, value.seconds);
+}
+
+TEST(ntp, bad_hour_value){
+    EXPECT_THROW(from_iso8601("1900-01-01 100:99:01"), std::exception);
+}
+
+TEST(ntp, bad_minute_value){
+    EXPECT_THROW(from_iso8601("1900-01-01 00:100:01"), std::exception);
+}
+
+TEST(ntp, bad_second_value){
+    EXPECT_NO_THROW(from_iso8601("1900-01-01 00:00:100000"));
+}
+
+TEST(ntp, bad_value_good_format){
+    EXPECT_NO_THROW(from_iso8601("9999-12-31 99:99:99.999999"));
+}
+
+TEST(ntp, bad_input){
+        EXPECT_THROW(from_iso8601("A1900-01-01 99:99
