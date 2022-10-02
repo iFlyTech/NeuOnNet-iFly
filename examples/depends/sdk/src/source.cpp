304ef166650900e0bec52fc0bf767e08e2335eb1
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-10-02 06:25:56 +0000

    initial framework

--- examples/depends/sdk/src/source.cpp
+++ examples/depends/sdk/src/source.cpp
@@ -11,4 +11,39 @@ extern "C" {
 #include <libavcodec/avcodec.h>
 }
 
-source_t::source_t(const std::string &filename, consu
+source_t::source_t(const std::string &filename, consumer video, consumer audio)
+: dmx{new ffmpeg::demuxer(filename)}
+{
+    setup(*dmx, video, audio);
+}
+
+source_t::~source_t() = default;
+
+void source_t::setup(ffmpeg::demuxer& dmx, const consumer &video, const consumer &audio) {
+    auto tracks = dmx.tracks();
+    //assert(tracks.size() == 2 && "Single Audio / Video streams only");
+
+    for (auto &&track : tracks) {
+
+        if(track.parameters.codec_type == AVMEDIA_TYPE_VIDEO){
+            vdec = std::make_shared<ffmpeg::decoder>(track.parameters);
+            decoders[track.index] = vdec;
+            consumers[track.index] = video;
+        } else if(track.parameters.codec_type == AVMEDIA_TYPE_AUDIO){
+            adec = std::make_shared<ffmpeg::decoder>(track.parameters);
+            decoders[track.index] = adec;
+            consumers[track.index] = audio;
+        }
+    }
+}
+
+void source_t::run(threads::interruption_t& interruption) {
+    auto au = dmx->get();
+    while( !interruption.done() && au ){
+
+        auto idx = au->packet->stream_index;
+        auto&& dec = decoders[idx];
+
+        dec->put(au->packet.get());
+        while(auto frame = dec->get()){
+  
