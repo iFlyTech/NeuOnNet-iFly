568ca293173e386dc72a32cd83ef17ec7a132d01
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-12-02 18:50:04 +0000

    Prepare 1.1.1 release

--- sources/src/details/neuon/details/dataset.py
+++ sources/src/details/neuon/details/dataset.py
@@ -20,4 +20,22 @@ def main():
 
     n = datalist.shape[0]
 
-    with h5py.File(options.dataset, mode='w') 
+    with h5py.File(options.dataset, mode='w') as dataset:
+        video_dataset = dataset.require_dataset('video', shape=(n, 9, 60, 100, 1), dtype='uint8', chunks=(32, 9, 60, 100, 1), maxshape=(None, 9, 60, 100, 1), compression="gzip", compression_opts=2)
+        video_dataset.dims[0].label = 'n'
+        video_dataset.dims[1].label = 'time'
+        video_dataset.dims[2].label = 'height'
+        video_dataset.dims[3].label = 'width'
+        video_dataset.dims[4].label = 'channels'
+
+        audio_dataset = dataset.require_dataset('audio', shape=(n, 3, 45, 15, 1), dtype='float',  chunks=(32, 3, 45, 15, 1), maxshape=(None, 3, 45, 15, 1), compression="gzip", compression_opts=2)
+        audio_dataset.dims[0].label = 'n'
+        audio_dataset.dims[1].label = 'derivation'
+        audio_dataset.dims[2].label = 'time'
+        audio_dataset.dims[3].label = 'coefficients'
+        audio_dataset.dims[4].label = 'channels'
+
+        distance_dataset = dataset.require_dataset('distance', shape=(n, ), dtype='uint8',  chunks=True, maxshape=(None, ), compression="gzip", compression_opts=9)
+        distance_dataset.dims[0].label = 'n'
+
+        timestamp_dataset = dataset.require_dataset('timestamps', shape=(n, 4), dty
