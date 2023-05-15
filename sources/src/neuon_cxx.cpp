05d0b8ac52751c63801372f322ac71fdff86dcb6
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2023-05-15 18:44:11 +0000

    PM / sleep: Add state field to pm_states[] entries

--- sources/src/neuon_cxx.cpp
+++ sources/src/neuon_cxx.cpp
@@ -15,4 +15,31 @@ neuon::neuon_t::~neuon_t() = default;
 
 void neuon::neuon_t::put(const neuon::audio_sample_t &sample) {
     std::vector<double> frame(sample.samples, 0.0);
-    for (int
+    for (int i = 0; i < sample.samples; ++i) {
+        const size_t bytes_per_sample = sizeof(double);
+        frame[i] = *reinterpret_cast<const double *>(&sample.p[i * bytes_per_sample]);
+    }
+
+    auto speech_fragments = speech_detection->detect(frame, sample.pts);
+
+    for (auto &fragment : speech_fragments) {
+        auto &timestamp = fragment.first;
+        auto &speech = fragment.second;
+        composer->add_audio(timestamp, std::move(speech));
+    }
+}
+
+void neuon::neuon_t::put(const neuon::video_sample_t &sample) {
+    dlib::array2d<uint8_t> picture(sample.height, sample.width);
+    for (int i = 0; i < sample.height; ++i) {
+        for (int j = 0; j < sample.width; ++j) {
+            picture[i][j] = sample.p[i * sample.stride + j];
+        }
+    }
+
+    auto mouth = face_detection->detect(picture);
+    composer->add_video(sample.pts, std::move(mouth));
+}
+
+uint64_t neuon::target_audio_samplerate(const std::chrono::microseconds& video_frame_duration) {
+    const std::chrono::microseconds audio_slice_estimation(video_frame_duration * neuon::video_frames
