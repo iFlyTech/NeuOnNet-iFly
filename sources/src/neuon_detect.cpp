2440b790983dfee1921dde04347229f08f336e89
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2023-07-13 13:53:04 +0000

    improved docs

--- sources/src/neuon_detect.cpp
+++ sources/src/neuon_detect.cpp
@@ -20,4 +20,26 @@
 #include <boost/filesystem.hpp>
 
 int main(int argc, char **argv){
-    std::string media_filen
+    std::string media_filename;
+    std::string model_filename;
+
+    std::string face_landmark_db("shape_predictor_68_face_landmarks.dat");
+    std::string license_filename("neuon.key");
+
+    std::cout << "neuon-detect :: " << version << " :: " << birthday << std::endl;
+    namespace po = boost::program_options;
+    po::options_description opt_desc( "Options" );
+    opt_desc.add_options( )
+                ( "help,h", "Produce this message" )
+                ( "input-media,i", po::value(&media_filename)->required(), "Input media clip to detect A/V sync" )
+                ( "model,m", po::value(&model_filename)->required(), "Input media clip to detect A/V sync" )
+                ( "landmarks,l", po::value(&face_landmark_db)->required()->default_value(face_landmark_db), "Face landmarks DB")
+                ( "license,", po::value(&license_filename), "License file")
+        ;
+
+    logging::verbosity = logging::verbosity_t::info;
+
+    po::positional_options_description pos_opt_desc;
+    po::variables_map varmap;
+    if( !options::is_args_valid(argc, argv, opt_desc, pos_opt_desc, varmap, std::cerr, std::cout ) ) {
+        retu
