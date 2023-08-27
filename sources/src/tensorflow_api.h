f66b7c0fdebddf8936170b35c732a85ee231273f
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2023-08-27 15:29:24 +0000

    00_SIGNALduino.pm

--- sources/src/tensorflow_api.h
+++ sources/src/tensorflow_api.h
@@ -7,4 +7,22 @@
 #include <vector>
 
 namespace tensorflow{
-    using buffer_pt
+    using buffer_ptr = std::unique_ptr<TF_Buffer, std::function<void(TF_Buffer *p)>>;
+    using import_graph_def_options_ptr = std::unique_ptr<TF_ImportGraphDefOptions, std::function<void(TF_ImportGraphDefOptions*)>>;
+    using status_ptr = std::unique_ptr<TF_Status, std::function<void(TF_Status *p)>>;
+    using session_options_ptr = std::unique_ptr<TF_SessionOptions, std::function<void(TF_SessionOptions*)>>;
+    using graph_ptr = std::unique_ptr<TF_Graph, std::function<void(TF_Graph *)>>;
+    using session_ptr = std::unique_ptr<TF_Session, std::function<void(TF_Session *)>>;
+    using tensor_ptr = std::unique_ptr<TF_Tensor, std::function<void(TF_Tensor *)>>;
+
+    class api_t{
+    public:
+        virtual ~api_t() = default;
+        virtual graph_ptr TF_NewGraph() = 0;
+        virtual import_graph_def_options_ptr TF_NewImportGraphDefOptions() = 0;
+        virtual session_options_ptr TF_NewSessionOptions() = 0;
+        virtual status_ptr TF_NewStatus() = 0;
+        virtual TF_Code TF_GetCode(status_ptr& s) = 0;
+        virtual const char *TF_Message(status_ptr& s) = 0;
+
+        virtual buffer_ptr TF_NewBufferFromString(const void*
